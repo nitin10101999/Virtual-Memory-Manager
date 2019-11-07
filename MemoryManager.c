d135e58c5745fc266de7e92c2f23800ba4ef6051
nitin10101999 <nitin10101999@gmail.com>
2019-11-07 19:15:24 +0530

    modified:   MemoryManager.c 	deleted:    lab7.c

--- MemoryManager.c
+++ MemoryManager.c
@@ -1,73 +1,145 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-
-#define empty -1
-#define disk -2
-#define NO_OF_PAGE 256
-#define ENTRIES_IN_TLB 16
+#define TLB_SIZE 16
+#define PAGE_TABLE_SIZE 256
+#define NA -1
 #define BUFF_SIZE 50
-
-FILE * addresses; 
-FILE * disk_file; 
-int pageTable_page[NO_OF_PAGE];
-int pageTable_frame[NO_OF_PAGE];
-int tlb_pageno[ENTRIES_IN_TLB];
-int tlb_frameno[ENTRIES_IN_TLB];
-int PM[NO_OF_PAGE][NO_OF_PAGE];
-int entry_in_TLB=0;
-int TLB_hit = 0;
-int page_fault=0;
-int current_page =0;
-int current_frame =0;
-int offset;
-void initialize_pageTable(){
-    for(int i=0;i<NO_OF_PAGE;i++){
-        pageTable_page[i]=-1;
+#define SIZE 256
+int TLB[TLB_SIZE][2];
+int Page_Table[PAGE_TABLE_SIZE];
+int Physical_Memory[PAGE_TABLE_SIZE][SIZE];
+int current_PM_loc = 0;
+int current_TLB_loc = 0;
+int TLB_hit = 0, Page_Table_hit = 0;
+// input file and backing store
+FILE *address_file;
+FILE *backing_store;
+void initialize()
+{
+    for (int i = 0; i < TLB_SIZE; i++)
+    {
+        TLB[i][0] = NA;
     }
-    for(int i=0;i<ENTRIES_IN_TLB;i++){
-        tlb_pageno[i]=-1;
+    for (int i = 0; i < PAGE_TABLE_SIZE; i++)
+    {
+        Page_Table[i] = NA;
     }
 }
-int find_in_TLB(int pageNumber){
-    for(int i=0;i<ENTRIES_IN_TLB;i++){
-        if(tlb_pageno[i]==pageNumber){
-            return tlb_frameno[i];
+void handle_Page_Table(int PageNo)
+{
+    if (Page_Table[PageNo] == NA)
+    {
+        /////page fault
+        char buffer[SIZE];
+        if (fseek(backing_store, PageNo * SIZE, SEEK_SET) != 0)
+        {
+            fprintf(stderr, "Error seeking in backing store\n");
         }
-    }
-    return -1;
-}
-int convertToPA(int LA){
-    int pageNumber,frameNumber;
-    pageNumber = LA/256;
-    offset = LA%256;
-    frameNumber = find_in_TLB(pageNumber);
-    if(frameNumber!=-1){
-        TLB_hit++;
-        return frameNumber;
-    }
-    else{
-        for(int i=0;i<NO_OF_PAGE;i++){
-            if(pageTable_page[i]==pageNumber){
-                frameNumber = pageTable_frame[i];
-            }
-            if(frameNumber==-1){
-                /* I have to write from here */
-                /* let's go  to study */
-            }
+        if (fread(buffer, sizeof(signed char), SIZE, backing_store) == 0)
+        {
+            fprintf(stderr, "Error reading from backing store\n");
         }
+        for (int i = 0; i < SIZE; i++)
+        {
+            Physical_Memory[current_PM_loc][i] = buffer[i];
+        }
+        Page_Table[PageNo] = current_PM_loc; ///// insert in page Table
+        current_PM_loc++;
+    }
+    else
+    {
+        Page_Table_hit++;
     }
 }
-int main(int argc, char *argv[]){
-    char addressToBeRead[50]; 
-    int LA;
-    int PA;
-    initialize_pageTable();
-    addresses = fopen(argv[1], "r");
-    disk_file = fopen("BACKING_STORE.bin", "rb");
-    while ( fgets(addressToBeRead, BUFF_SIZE, addresses) != NULL) {
-        LA = atoi(addressToBeRead);
-        PA = convertToPA(LA);   
+int handle_TLB(int PageNo)
+{
+    int frame = -1;
+    for (int i = 0; i < TLB_SIZE; i++)
+    {
+        if (TLB[i][0] == PageNo)
+        {
+            frame = TLB[i][1];
+            TLB_hit++;
+            Page_Table_hit++;
+            return frame;
+        }
+        
+    }
+    if (frame == -1)
+    {
+        handle_Page_Table(PageNo);
+        if (current_TLB_loc >= 0 && current_TLB_loc < TLB_SIZE)
+        {
+            /////inset the page fault value
+            TLB[current_TLB_loc][0] = PageNo;
+            TLB[current_TLB_loc][1] = Page_Table[PageNo];
+            frame = Page_Table[PageNo];
+            current_TLB_loc++;
+        }
+        else
+        {
+            for (int i = 1; i < TLB_SIZE; i++)
+            {
+                TLB[i - 1][0] = TLB[i][0];
+                TLB[i - 1][1] = TLB[i][1];
+            }
+            //current_TLB_loc--;
+            /////inset the page fault value
+            TLB[TLB_SIZE-1][0] = PageNo;
+            TLB[TLB_SIZE-1][1] = Page_Table[PageNo];
+            frame = Page_Table[PageNo];
+        }
+        
+    }
+    return frame;
+}
+int findPage(int PageNo)
+{
+    int frame = handle_TLB(PageNo);
+    return frame;
+}
+int main(int argc, char *argv[])
+{
+    initialize();
+    backing_store = fopen("BACKING_STORE.bin", "rb");
+
+    if (backing_store == NULL)
+    {
+        fprintf(stderr, "Error opening BACKING_STORE.bin %s\n", "BACKING_STORE.bin");
+        return -1;
     }
 
+    address_file = fopen(argv[1], "r");
+
+    if (address_file == NULL)
+    {
+        fprintf(stderr, "Error opening addresses.txt %s\n", argv[1]);
+        return -1;
+    }
+    char address[BUFF_SIZE];
+    int LA, frame, d, PageNo;
+    float count = 0.0;
+    FILE *fptr;
+    fptr = fopen("correct.txt", "w");
+    while (fgets(address, BUFF_SIZE, address_file) != NULL)
+    {
+        LA = atoi(address);
+        LA = LA % 65536; /////only sixteen bit;
+        PageNo = LA / 256;
+        d = LA % 256;
+        frame = findPage(PageNo);
+        count++;
+
+        //printf("%d\n",(frame << 8) | d);
+
+        fprintf(fptr,"Virtual Address: %d and Physical Address:%d\n",LA,(frame << 8) | d);
+    }
+    printf("TLB hit:%d\n",TLB_hit);
+    printf("TLB hit rate:%.4f\n",TLB_hit/count);
+    printf("Page fault:%d\n",1000-Page_Table_hit);
+    printf("Page  fault rate:%.4f\n",(1000-Page_Table_hit)/count);
+    fclose(address_file);
+    fclose(backing_store);
+    fclose(fptr);
 }
